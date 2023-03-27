0060fcf44f6ed27eea236c5780fc387fc9db5b82
dpakp860 <94121946+dpakp860@users.noreply.github.com>
2023-03-27 21:14:27 +0530

    Add files via upload

---
+++ MultipleLanguage.java
@@ -0,0 +1,10 @@
+package week6.day1;
+
+public abstract class MultipleLanguage implements Language,TestTool{
+
+	public void python(){
+		System.out.println("PyThOn");
+		
+	}
+	public abstract  void ruby();
+}
